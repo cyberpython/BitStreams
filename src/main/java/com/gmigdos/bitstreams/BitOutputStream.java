d5e7779d850723d9f5c62bf0c05633009f6df9b6
Giorgos Migdos <cyberpython@gmail.com>
2015-11-30 19:50:32 +0200

    Migrate to gradle

--- src/main/java/com/gmigdos/bitstreams/BitOutputStream.java
+++ src/main/java/com/gmigdos/bitstreams/BitOutputStream.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package bitstreams;
+package com.gmigdos.bitstreams;
 
 import java.io.FilterOutputStream;
 import java.io.IOException;
